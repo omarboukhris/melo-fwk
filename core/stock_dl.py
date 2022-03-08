2dbef51adac9043c651116aa60a8691a9ddbf8bd
omar <omarboukhris@gmail.com>
2022-03-08 18:12:59 +0100

    added eurusd and different time windows

--- core/stock_dl.py
+++ core/stock_dl.py
@@ -6,7 +6,7 @@ def download_and_store(stcks, fnames):
 	for stock, fn in zip(stcks, fnames):
 		tick = yf.Ticker(stock)
 
-		df = tick.history(period="10y", interval="1d")
+		df = tick.history(period="1mo", interval="5m")
 		df.to_csv(f"{fn}.csv")
 
 		print(tick.history().columns)
@@ -15,7 +15,13 @@ def download_and_store(stcks, fnames):
 
 if __name__ == "__main__":
 
-	stocks = ["AAPL", "GOOG", "BTC-EUR", "ETH-EUR"]
-	filenames = ["data/AAPL", "data/GOOG", "data/BTCEUR", "data/ETHEUR"]
+	stocks = ["AAPL", "GOOG", "BTC-EUR", "ETH-EUR", "EURUSD=X"]
+	filenames = ["data/AAPL", "data/GOOG", "data/BTCEUR", "data/ETHEUR", "data/EURUSD"]
 
-	download_and_store(stocks, filenames)
+	idx, ext_id = 4, 2
+	ext = ["_1d_10y", "1h_2y", "5m_1mo"]
+
+	download_and_store(
+		[stocks[idx]],
+		[filenames[idx] + ext[ext_id]]
+	)
