06c3ce795a97e315a97c83d8be47c097352bfa1c
omar <omarboukhris@gmail.com>
2023-06-26 19:21:02 +0200

    introduce melomachina

--- tests/test_mql_process.py
+++ tests/test_mql_process.py
@@ -16,77 +16,85 @@ from mql.mql_parser import MqlParser
 from pathlib import Path
 
 
-def run_mql_process(mql_query_path: Path):
-	"""
-	Estimators should:
-		- Implement the same constructor (see any estimator)
-		- Implement a run() method that returns any result
-	Note:
-		Reporter associated to the estimator should be able
-		to process its result
+class MeloMachina:
+	def __init__(self, config: Path, loggers: list):
+		# setup config
+		GenericConfigLoader.setup(str(config))
+		# setup logger
+		GlobalLogger.set_loggers(loggers)
+		# setup mql parser
+		self.mql_parser = MqlParser()
+		# register melo components in factory
+		quantfactory_registry.register_all()
 
-	TODO:
-		Add estimator config load from toml file
-		+ default config load from some assets/folder
-	"""
+	def run_mql_process(self, mql_query_path: Path):
+		"""
+		Estimators should:
+			- Implement the same constructor (see any estimator)
+			- Implement a run() method that returns any result
+		Note:
+			Reporter associated to the estimator should be able
+			to process its result
+			TODO: rework reporters with new persistency layer
+		"""
 
-	mql_parser = MqlParser()
-	parsed_mql = mql_parser.parse_to_json(str(mql_query_path))
-	quant_query = parsed_mql["QuantQuery"][0]
-	# print(parsed_mql)
+		parsed_mql = self.mql_parser.parse_to_json(str(mql_query_path))
+		quant_query = parsed_mql["QuantQuery"][0]
+		# print(parsed_mql)
 
-	work_dir = Path(GenericConfigLoader.get_node("working_directory"))
-	if "Clusters" in quant_query.keys():
-		pf_mgr = CompositePortfolioManager.from_config(GenericConfigLoader.get_node(CompositePortfolioManager.__name__))
-		market_mgr = CompositeMarketLoader.from_config(GenericConfigLoader.get_node(CompositeMarketLoader.__name__))
-		mql_clusters_config = MeloClustersConfig.build_config(pf_mgr, market_mgr, quant_query)
-		cluster_estim_ = mql_clusters_config.build_clusters_estimator()
-		output = cluster_estim_.run()
-		mql_clusters_config.write_report(output, str(mql_query_path.parent))
+		if "Clusters" in quant_query.keys():
+			pf_mgr = CompositePortfolioManager.from_config(GenericConfigLoader.get_node(CompositePortfolioManager.__name__))
+			market_mgr = CompositeMarketLoader.from_config(GenericConfigLoader.get_node(CompositeMarketLoader.__name__))
+			mql_clusters_config = MeloClustersConfig.build_config(pf_mgr, market_mgr, quant_query)
+			cluster_estim_ = mql_clusters_config.build_clusters_estimator()
+			output = cluster_estim_.run()
+			mql_clusters_config.write_report(output, str(mql_query_path.parent))
 
-	else:
-		mql_config = MeloConfig.build_config(
-			quant_query_path=mql_query_path,
-			quant_query=quant_query
-		)
-		# print(mql_config)
+		else:
+			mql_config = MeloConfig.build_config(
+				quant_query_path=mql_query_path,
+				quant_query=quant_query
+			)
+			# print(mql_config)
 
-		estimator_obj_ = mql_config.build_estimator()
-		output = estimator_obj_.run()
-		# print(output)
+			estimator_obj_ = mql_config.build_estimator()
+			output = estimator_obj_.run()
+			# print(output)
 
-		mql_config.write_report(output, str(mql_query_path.parent))
+			mql_config.write_report(output, str(mql_query_path.parent))
 
-		pf_mgr = CompositePortfolioManager.from_config(GenericConfigLoader.get_node("CompositeMarketLoader"))
-		mql_config.export_trading_system(pf_mgr)
+			pf_mgr = CompositePortfolioManager.from_config(GenericConfigLoader.get_node("CompositeMarketLoader"))
+			mql_config.export_trading_system(pf_mgr)
 
 
 class MqlUnitTests(unittest.TestCase):
 
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.config = Path(__file__).parent / "rc/config.json"
+		self.loggers = [ConsoleLogger]
+
 	def test_mql_process(self):
 
+		root_dir = Path(__file__).parent.parent
 		templates = {
-			"alloc": Path(__file__).parent.parent / "mql/data/mql_alloc_optim_template/allocationoptim_example_query.sql",
-			"var": Path(__file__).parent.parent / "mql/data/mql_var_template/var_example_query.sql",
-			"backtest": Path(__file__).parent.parent / "mql/data/mql_backtest_template/backtest_example_query.sql",
-			"fw_opt": Path(__file__).parent.parent / "mql/data/mql_forecast_weights_optim/forecastweightsoptim_example_query.sql",
-			"vol_target_opt": Path(__file__).parent.parent / "mql/data/mql_vol_target_optim/posesizeoptim_example_query.sql",
-			"clustering": Path(__file__).parent.parent / "mql/data/mql_clustering_template/clustering_example_query.sql",
-			"fast_strat_opt": Path(__file__).parent.parent / "mql/data/mql_strat_opt_template/fast_stratoptim_example_query.sql",
+			"alloc": root_dir / "mql_data/mql_alloc_optim_template/allocationoptim_example_query.sql",
+			"var": root_dir / "mql_data/mql_var_template/var_example_query.sql",
+			"backtest": root_dir / "mql_data/mql_backtest_template/backtest_example_query.sql",
+			"fw_opt": root_dir / "mql_data/mql_forecast_weights_optim/forecastweightsoptim_example_query.sql",
+			"vol_target_opt": root_dir / "mql_data/mql_vol_target_optim/posesizeoptim_example_query.sql",
+			"clustering": root_dir / "mql_data/mql_clustering_template/clustering_example_query.sql",
+			"fast_strat_opt": root_dir / "mql_data/mql_strat_opt_template/fast_stratoptim_example_query.sql",
 		}
 		# still missing :
 		# alloc opt
 
 		# set loggers
-		GlobalLogger.set_loggers([ConsoleLogger])
-		GenericConfigLoader.setup(str(Path(__file__).parent / "rc/config.json"))
-
-		# register melo components in factory
-		quantfactory_registry.register_all()
+		mm = MeloMachina(config=self.config, loggers=self.loggers)
 
 		for key, mql_query in templates.items():
 			print(42*"=" + key + 42*"=")
-			run_mql_process(mql_query_path=mql_query)
+			mm.run_mql_process(mql_query_path=mql_query)
 
 
 if __name__ == "__main__":
